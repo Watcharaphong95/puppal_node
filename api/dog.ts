c4eeed318d1ddd1aee71dfbed6dfd327cafc7028
Watcharaphong <65011212077@msu.ac.th>
2025-01-07 03:38:52 +0700

    add dog regis

--- api/dog.ts
+++ api/dog.ts
@@ -18,34 +18,33 @@ router.get("/", (req, res) => {
 });
 
 router.get("/breed", (req, res) => {
-    let sql = "SELECT * FROM breed";
-  
-    conn.query(sql, (err, result) => {
-      if (err) {
-        res.status(400).json({ msg: err.message });
-      } else {
-        res.status(200).json(result);
-      }
-    });
+  let sql = "SELECT * FROM breed ORDER BY name COLLATE utf8mb4_unicode_520_ci;";
+
+  conn.query(sql, (err, result) => {
+    if (err) {
+      res.status(400).json({ msg: err.message });
+    } else {
+      res.status(200).json(result);
+    }
   });
+});
 
 router.get("/:uid", (req, res) => {
-    let uid = req.params.uid;
+  let uid = req.params.uid;
 
-    let sql = "SELECT dog.*, dog.name AS dogName, breed.name AS breed FROM dog, breed WHERE dog.b_did = breed.bid AND u_did = ?";
+  let sql =
+    "SELECT dog.*, dog.name AS dogName, breed.name AS breed FROM dog, breed WHERE dog.b_did = breed.bid AND u_did = ?";
 
-    sql = mysql.format(sql, [
-        uid
-    ])
-  
-    conn.query(sql, (err, result) => {
-      if (err) {
-        res.status(400).json({ msg: err.message });
-      } else {
-        res.status(200).json(result);
-      }
-    });
+  sql = mysql.format(sql, [uid]);
+
+  conn.query(sql, (err, result) => {
+    if (err) {
+      res.status(400).json({ msg: err.message });
+    } else {
+      res.status(200).json(result);
+    }
   });
+});
 
 router.post("/register", (req, res) => {
   let dogData: DogData = req.body;
@@ -53,31 +52,29 @@ router.post("/register", (req, res) => {
   let sql =
     "INSERT INTO dog (b_did, u_did, name, gender, color, defect, birth, conDisease, vacHistory, sterilization, pic, hair, status) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?) ";
   sql = mysql.format(sql, [
-    dogData.b_did,
-    dogData.u_did,
-    dogData.name,
-    dogData.gender,
-    dogData.color,
-    dogData.defect,
-    dogData.birth,
-    dogData.conDisease,
-    dogData.vacHistory,
-    dogData.sterilization,
-    dogData.pic,
-    dogData.hair,
-    dogData.status,
+    dogData.b_did || null,
+    dogData.u_did || null,
+    dogData.name || null,
+    dogData.gender || null,
+    dogData.color || null,
+    dogData.defect || null,
+    dogData.birth || null,
+    dogData.conDisease || null,
+    dogData.vacHistory || null,
+    dogData.sterilization || null,
+    dogData.pic || null,
+    dogData.hair || null,
+    dogData.status || null,
   ]);
 
   conn.query(sql, (err, result) => {
     if (err) {
       res.status(400).json({ msg: err.message });
     } else {
-      res
-        .status(200)
-        .json({
-          affected_rows: result.affectedRows,
-          last_idx: result.insertId,
-        });
+      res.status(200).json({
+        affected_rows: result.affectedRows,
+        last_idx: result.insertId,
+      });
     }
   });
 });
