19ff2a2c135612bdea4510ec8718ccd70c57f149
Watcharaphong <65011212077@msu.ac.th>
2025-01-07 06:23:32 +0700

    fix

--- api/reserve.ts
+++ api/reserve.ts
@@ -48,7 +48,7 @@ router.post("/add", (req, res) => {
 router.get("/:did", (req, res) => {
   let did = req.params.did;
 
-  let sql = "SELECT reserve.d_rid, dog.pic AS dogPic, dog.name AS dogname, user.username AS ownername, user.profilePic AS owmerPic, reserve.date, reserve.status FROM reserve, user, dog WHERE reserve.u_rid = user.uid AND reserve.d_rid = dog.did AND doc_rid = ? AND reserve.status = ?";
+  let sql = "SELECT reserve.rid, reserve.d_rid, dog.pic AS dogPic, dog.name AS dogname, user.username AS ownername, user.profilePic AS owmerPic, reserve.date, reserve.status FROM reserve, user, dog WHERE reserve.u_rid = user.uid AND reserve.d_rid = dog.did AND doc_rid = ? AND reserve.status = ?";
 
   sql = mysql.format(sql, [did, 0]);
 
